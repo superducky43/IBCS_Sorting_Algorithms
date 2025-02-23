7eb913756dc0859986940af0527f5226094b573a
superducky43 <119814711+superducky43@users.noreply.github.com>
2025-02-23 21:26:13 +0100

    done both necessary algorithms

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,4 +1,59 @@
-package PACKAGE_NAME;
+public abstract class Algorithms
+{
+    static int[] bubbleSort(int[] array)
+    {
+        int firstElement;
+        int secondElement;
+        boolean flag = true;
 
-public class Algorithms {
+        while (flag)
+        {
+            flag = false;
+
+            //array.length - 1 because element i+1 is accessed in for loop
+            for (int i = 0; i < array.length - 1; i++)
+            {
+                firstElement = array[i];
+                secondElement = array[i + 1];
+
+                if (firstElement > secondElement)
+                {
+                    array[i] = secondElement;
+                    array[i + 1] = firstElement;
+                    flag = true;
+                }
+            }
+        }
+
+        return array;
+    }
+
+    static int[] selectionSort(int[] array)
+    {
+        int smallestElementIndex;
+        int selectedElementIndex;
+
+        /*
+        Array.length - 1 because there should always still be
+        1 spot left to compare the current/assumed smallestElementIndex to
+         */
+        for (int i = 0; i < array.length - 1; i++)
+        {
+            smallestElementIndex = i;
+
+            for (int j = i + 1; j < array.length; j++)
+            {
+                selectedElementIndex = array[j];
+
+                if (selectedElementIndex < smallestElementIndex)
+                {
+                    int temporaryElement = array[smallestElementIndex];
+                    array[smallestElementIndex] = array[selectedElementIndex];
+                    array[selectedElementIndex] = temporaryElement;
+                }
+            }
+        }
+
+        return array;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int[] array = {3,4,8,1,5,2,10,25,5,2};
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Bubble sort:      " + Arrays.toString(Algorithms.bubbleSort(array)));
+        System.out.println("Selection sort:   " + Arrays.toString(Algorithms.selectionSort(array)));
     }
 }
